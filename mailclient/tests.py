07ef00182827a80dd7dba7046510b9739a51fdaa
Adrian Espinosa <aespinosamoreno@gmail.com>
2013-08-26 19:08:05 +0200

    Tests adapted

--- mailclient/tests.py
+++ mailclient/tests.py
@@ -4,7 +4,8 @@
 Tests for mail client
 """
 
-import mail
+import server
+import message
 import unittest
 from mail_exceptions import ConnectionRefused, DataError, InvalidPath
 
@@ -20,50 +21,50 @@ class MailClientTestCase(unittest.TestCase):
 
     def test_connection_refused(self):
         with self.assertRaises(ConnectionRefused):
-            mail.Server('localhost_fake')
+            server.Server('localhost_fake')
 
     def test_connection_ok(self):
-        s = mail.Server('smtp.gmail.com', 587)
-        self.assertIsInstance(s, mail.Server)
+        s = server.Server('smtp.gmail.com', 587)
+        self.assertIsInstance(s, server.Server)
 
     def test_send_string(self):
-        s = mail.Server('localhost')
+        s = server.Server('localhost')
         with self.assertRaises(DataError):
             s.send("This is a string")
 
     def test_send_ok(self):
-        s = mail.Server('localhost')
-        msg = mail.Message('This is my subject', 'And this is the body',
+        s = server.Server('localhost')
+        msg = message.Message('This is my subject', 'And this is the body',
                            'iamthe@sender.com', 'iamthe@recipient1.com, '
                                                 'iamthe@recipient2.com')
         s.send(msg)
 
     def test_file_int(self):
-        msg = mail.Message()
+        msg = message.Message()
         with self.assertRaises(DataError):
             msg.attach(1234)
 
     def test_file_file(self):
         fp = open('mail_exceptions.py')
-        msg = mail.Message()
+        msg = message.Message()
         msg.attach(fp)
 
     def test_file_string(self):
-        msg = mail.Message()
+        msg = message.Message()
         msg.attach('mail_exceptions.py')
 
     def test_file_path_bad(self):
-        msg = mail.Message()
+        msg = message.Message()
         with self.assertRaises(InvalidPath):
             msg.attach('/this/does/not/exist/')
 
     def test_file_path_ok(self):
-        msg = mail.Message()
+        msg = message.Message()
         msg.attach('mail_exceptions.py')
 
     def test_adding_name(self):
-        msg = mail.Message(sender="'Adrian Espinosa' me@me.com")
-        s = mail.Server('localhost')
+        msg = message.Message(sender="'Adrian Espinosa' me@me.com")
+        s = server.Server('localhost')
         s.send(msg)
 
 
